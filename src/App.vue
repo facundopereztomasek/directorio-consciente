c8df4bdd962cfd279e215582383972bb870a4f19
Facundo Pérez Tomasek <facundopereztomasek@gmail.com>
2021-03-15 12:13:45 -0300

    directorio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <h1>Directorio Consciente</h1>
+    <hr />
+    <div class="content-item-title">
+      <div class="image-wrapper">
+        <img
+          class="image"
+          :src="require('@/assets/vejolk.jpg')"
+          alt="Vjolk Distribuidora"
+        />
+      </div>
+      <div>
+        <h2 class="title">Vejølk</h2>
+        <h3 class="subtitle">
+          Distribuidora de Alimentos y articulos exclusivamente Veganos
+        </h3>
+      </div>
+    </div>
+    <div class="content-item-data">
+      <p class="label">Teléfono</p>
+      <p class="data">
+        <a
+          ref="noopener noreferrer"
+          target="_blank"
+          href="tel:+54 9 11 5095 2174"
+          >123-456-7890</a
+        >
+      </p>
+      <p class="label">Instagram</p>
+      <p class="data">
+        <a
+          ref="noopener noreferrer"
+          target="_blank"
+          href="https://www.instagram.com/vejolk"
+          >@vejolk</a
+        >
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1,
+h2,
+h3,
+p {
+  margin: 0;
+}
+
+.main {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  .content-item-title {
+    margin-bottom: 20px;
+  }
+
+  .content-item-title,
+  .content-item-data {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+    grid-gap: 10px 20px;
+    align-items: center;
+
+    & > * {
+      &:nth-child(even) {
+        justify-self: start;
+      }
+      &:nth-child(odd) {
+        justify-self: end;
+      }
+    }
+  }
+  .image-wrapper {
+    overflow: hidden;
+    border-radius: 100%;
+    .image {
+      width: 60px;
+      height: 60px;
+      vertical-align: bottom;
+    }
+  }
 }
 </style>
